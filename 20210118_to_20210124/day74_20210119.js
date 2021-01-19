c36b2604ad7aaab2399afc7167e9d657f6d5ceab
ken <298074606@qq.com>
2021-01-18 17:35:14 -0800

    20210119_0935

---
+++ 20210118_to_20210124/day74_20210119.js
@@ -0,0 +1,25 @@
+var arr1 = "ab".split(''); //=>[a,b]
+var arr2 = arr1.reverse(); //=>[b,a]
+var arr3 = "abc".split(''); //=>[a,b,c]
+arr2.push(arr3); //=>[b,a,[a,b,c]]
+console.log(arr1.length); //=>2
+console.log(arr1.slice(-1)); //=>b
+console.log(arr2.length); //=>3
+console.log(arr2.slice(-1)); //=>[a,b,c]
+
+/*
+// 答案
+3 ["a","b","c"] 3 ["a","b","c"]
+
+//解析
+这个题其实主要就是考察的reverse会返回该数组的引用，但是容易被迷惑，导致答错，如果知道这个点，就不会掉坑里了。
+
+1）reverse
+MDN 上对于 reverse() 的描述是酱紫的：
+The reverse method transposes the elements of the calling array object in place, mutating the array, and returning a reference to the array.
+reverse 方法颠倒数组中元素的位置，改变了数组，并返回该数组的引用。
+
+2）slice
+slice() 方法返回一个新的数组对象，这一对象是一个由 begin 和 end 决定的原数组的浅拷贝（包括 begin，不包括end）。原始数组不会被改变。
+如果该参数为负数， 则它表示在原数组中的倒数第几个元素结束抽取。 slice(-2,-1) 表示抽取了原数组中的倒数第二个元素到最后一个元素（不包含最后一个元素，也就是只有倒数第二个元素）。
+*/
---
+++ 20210118_to_20210124/day75_20210119.js
@@ -0,0 +1,34 @@
+var F = function() {}
+Object.prototype.a = function() {
+    console.log('yideng')
+}
+Function.prototype.b = function() {
+    console.log('xuetang')
+}
+var f = new F()
+F.a(); //'yideng'
+F.b(); //'xuetang'
+f.a(); //no function
+f.b(); //xuetang
+
+/*
+// 答案
+yideng xuetang yideng 报错
+//解析
+1）F.a();F.b();
+F是个构造函数，而f是构造函数F的一个实例。
+因为F instanceof Object == true、F instanceof Function == true
+由此我们可以得出结论：F是Object 和 Function两个的实例，即F既能访问到a，也能访问到b。
+所以F.a() 输出 yideng F.b() 输出 xuetang
+
+2）f.a();f.b();
+对于f，我们先来看下下面的结果：
+f并不是Function的实例，因为它本来就不是构造函数，调用的是Function原型链上的相关属性和方法了，只能访问Object原型链。
+所以f.a() 输出 yideng 而f.b()就报错了。
+
+3）具体分析下它们是如何按路径查找的：
+①f.a的查找路径: f自身: 没有 ---> f.__proto__(Function.prototype),没有--->f.__proto__.__proto__(Object.prototype): 输出yideng
+②f.b的查找路径: f自身: 没有 ---> f.__proto__(Function.prototype): 没有 ---> f.__proto__.__proto__ (Object.prototype): 因为找不到，所以报错
+③F.a的查找路径: F自身: 没有 ---> F.__proto__(Function.prototype): 没有 ---> F.__proto__.__proto__(Object.prototype): 输出 yideng
+④F.b的查找路径: F自身: 没有 ---> F.__proto__(Function.prototype): xuetang
+*/
